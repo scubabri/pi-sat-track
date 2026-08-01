fe851739a3b6fa7e680ad3acb21bcca56cf1039d
scubabri <scubabri@gmail.com>
2026-08-01 08:54:48 -0600

    Restore catalog.js: binary-refine AOS to 1s after 30s coarse scan

--- node/sat-tracker/lib/catalog.js
+++ node/sat-tracker/lib/catalog.js
@@ -1,1 +1,16 @@
-PLACEHOLDER
+const fs = require("fs");
+const {
+  CACHE_DIR,
+  CATALOG_CACHE,
+  STATUS_CACHE,
+  CATALOG_URL,
+  AMSAT_STATUS,
+  DEFAULT_SAT,
+} = require("./config");
+const { getSatrecForNorad } = require("./tle");
+const { lookAngles } = require("./orbit");
+
+let CATALOG = {};
+let catalogNote = "not loaded";
+let ACTIVE = new Set();
+let statusNote = "not loaded";
